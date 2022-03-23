30b1bb07f512b61eb8af52333d71bcebbe405a40
gracenephele <gracenephele@naver.com>
2022-03-23 17:04:28 +0900

    220323 (1)

--- WinApi2D/CScene_Title.cpp
+++ WinApi2D/CScene_Title.cpp
@@ -30,24 +30,21 @@ void CScene_Title::Enter() {
 	//TODO: 
 	//1. 배경 출력, 
 
+	/*
 	CImageObject* backgroundObject = new CImageObject;
 	backgroundObject->Load(L"BackImage", L"texture\\background_start.png");
 	backgroundObject->SetPos(fPoint(0.f, 0.f));
 	backgroundObject->SetScale(fPoint(WINSIZEX, WINSIZEY));
 	AddObject(backgroundObject, GROUP_GAMEOBJ::BACKGROUND);
 
-	// 
-	// 
 	//2. 로고 출력용 오브젝트 제작,
-
-
  	CImageObject* logoObject = new CImageObject;
 	logoObject->Load(L"LogoImage", L"texture\\Kirby's-Adventure-Logo.png");
 	logoObject->SetPos(fPoint(100.f, 100.f));
 	logoObject->SetScale(fPoint(1080.f, 200.f));
 	AddObject(logoObject, GROUP_GAMEOBJ::BACKGROUND);
 
-
+	*/
 	//3. 시작 버튼, 
 	//4. 도움말 버튼 
 	//5. 종료 버튼
